a0327e48edb0b58aac672028958bfbd03be51e8c
Anna <anaumovich@exadel.com>
2023-01-04 14:40:40 +0100

    added test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+//
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import React from "react";
+import App from './App';
+import renderer from "react-test-renderer";
+
+test("First snapshot test ", ()=>{
+    const component = renderer.create(
+        <App/>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+})
