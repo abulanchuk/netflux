78c9067cf4117da8ac1e39b47887c3f8d54d3da1
Anna <anaumovich@exadel.com>
2023-01-05 16:38:26 +0100

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,15 +6,3 @@
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 // });
-
-import React from "react";
-import App from './App';
-import renderer from "react-test-renderer";
-
-test("First snapshot test ", ()=>{
-    const component = renderer.create(
-        <App/>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-})
